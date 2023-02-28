7e8a53e0a13a1c8900ae64a794d3944e6c1bad3c
abdallah-hatem <ahkortam@gmail.com>
2023-03-01 01:46:13 +0200

    commm

--- client/src/layout/NavBar/index.jsx
+++ client/src/layout/NavBar/index.jsx
@@ -85,6 +85,25 @@ export default function NavBar() {
     },
   ];
 
+  const settingsInputs = [
+    {
+      label: "number of devices",
+      type: "number",
+      name: "devices_number",
+      value: values["devices_number"],
+      onChange: handleChange,
+      style: { marginBottom: 15 },
+    },
+    {
+      label: "Hour Rate",
+      type: "number",
+      name: "hour_rate",
+      value: values["hour_rate"],
+      onChange: handleChange,
+      style: { marginBottom: 15 },
+    },
+  ];
+
   return (
     <div className="navbar-container">
       <h2>PlayStation.</h2>
@@ -131,21 +150,16 @@ export default function NavBar() {
         onCancel={() => setModalOpen(false)}
         onOk={handleSettingsButton}
       >
-        <InputComponent
-          label="number of devices"
-          type="number"
-          name="devices_number"
-          value={values["devices_number"]}
-          onChange={handleChange}
-          style={{ marginBottom: 15 }}
-        />
-        <InputComponent
-          label="Hour Rate"
-          type="number"
-          name="hour_rate"
-          value={values["hour_rate"]}
-          onChange={handleChange}
-        />
+        {settingsInputs.map((el) => (
+          <InputComponent
+            label={el.label}
+            type={el.type}
+            name={el.name}
+            value={el.value}
+            onChange={el.onChange}
+            style={el.style}
+          />
+        ))}
       </ModalComponent>
 
       <ModalComponent
